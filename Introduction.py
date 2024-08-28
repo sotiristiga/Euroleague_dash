71d11756f3b189d2f9ac2fd6ccd8ad1eedbaadd2
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-08-28 11:44:55 +0300

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,283 @@
 
+
+import requests
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+from math import ceil
+from datetime import date
+from streamlit_dynamic_filters import DynamicFilters
+import urllib.request
+from PIL import Image
+import time
+st.set_page_config(layout='wide',page_title="Euroleague")
+def fixture_format1(Fixture):
+    if Fixture<=15:
+        return "First Round"
+    elif Fixture>15 and Fixture<=30:
+        return "Second Round"
+    elif Fixture==31:
+        return "PO 1"
+    elif Fixture == 32:
+        return "PO 2"
+    elif Fixture == 33:
+        return "PO 3"
+    elif Fixture == 34:
+        return "PO 4"
+    elif Fixture == 35:
+        return "PO 5"
+    elif Fixture==36:
+        return "Semi Final"
+    elif Fixture==37:
+        return "Third Place"
+    elif Fixture==38:
+        return "Final"
+def fixture_format2(Fixture):
+    if Fixture <= 15:
+        return "First Round"
+    elif Fixture > 15 and Fixture <= 30:
+        return "Second Round"
+    elif Fixture == 31:
+        return "PO 1"
+    elif Fixture == 32:
+        return "PO 2"
+    elif Fixture == 33:
+        return "PO 3"
+    elif Fixture == 34:
+        return "PO 4"
+    elif Fixture == 35:
+        return "Semi Final"
+    elif Fixture == 36:
+        return "Third Place"
+    elif Fixture == 37:
+        return "Final"
+def fixture_format3(Fixture):
+    if Fixture <= 15:
+        return "First Round"
+    elif Fixture > 15 and Fixture <= 34:
+        return "Second Round"
+def fixture_format4(Fixture):
+    if Fixture <= 15:
+        return "First Round"
+    elif Fixture > 15 and Fixture <= 34:
+        return "Second Round"
+    elif Fixture == 35:
+        return "PO 1"
+    elif Fixture == 36:
+        return "PO 2"
+    elif Fixture == 37:
+        return "PO 3"
+    elif Fixture == 38:
+        return "PO 4"
+    elif Fixture == 39:
+        return "PO 5"
+    elif Fixture == 40:
+        return "Semi Final"
+    elif Fixture == 41:
+        return "Third Place"
+    elif Fixture == 42:
+        return "Final"
+
+def fixture_format5(Fixture):
+        if Fixture <= 15:
+            return "First Round"
+        elif Fixture > 15 and Fixture <= 34:
+            return "Second Round"
+        elif Fixture == 35:
+            return "PI 1"
+        elif Fixture == 36:
+            return "PI 2"
+        elif Fixture == 37:
+            return "PO 1"
+        elif Fixture == 38:
+            return "PO 2"
+        elif Fixture == 39:
+            return "PO 3"
+        elif Fixture == 40:
+            return "PO 4"
+        elif Fixture == 41:
+            return "PO 5"
+        elif Fixture == 42:
+            return "Semi Final"
+        elif Fixture == 43:
+            return "Third Place"
+        elif Fixture == 44:
+            return "Final"
+
+
+
+lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+
+def metrics_customize(red,green,blue,iconname,sline,i):
+
+    htmlstr = f"""<p style='background-color: rgb({red},{green},{blue}, 0.75); 
+                        color: rgb(0,0,0, 0.75); 
+                        font-size: 25px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 22px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+    return htmlstr
+euroleague_2016_2017_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2016_2017_playerstats.csv")
+euroleague_2016_2017_playerstats['idseason']=euroleague_2016_2017_playerstats['IDGAME'] + "_" + euroleague_2016_2017_playerstats['Season']
+euroleague_2016_2017_playerstats[['Fixture', 'Game']] = euroleague_2016_2017_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2016_2017_playerstats['Fixture']=pd.to_numeric(euroleague_2016_2017_playerstats['Fixture'])
+euroleague_2016_2017_playerstats['Round']=euroleague_2016_2017_playerstats['Fixture'].apply(fixture_format1)
+
+
+euroleague_2017_2018_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2017_2018_playerstats.csv")
+euroleague_2017_2018_playerstats['idseason']=euroleague_2017_2018_playerstats['IDGAME'] + "_" + euroleague_2017_2018_playerstats['Season']
+euroleague_2017_2018_playerstats[['Fixture', 'Game']] = euroleague_2017_2018_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2017_2018_playerstats['Fixture']=pd.to_numeric(euroleague_2017_2018_playerstats['Fixture'])
+euroleague_2017_2018_playerstats['Round']=euroleague_2017_2018_playerstats['Fixture'].apply(fixture_format2)
+
+
+euroleague_2018_2019_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2018_2019_playerstats.csv")
+euroleague_2018_2019_playerstats['idseason']=euroleague_2018_2019_playerstats['IDGAME'] + "_" + euroleague_2018_2019_playerstats['Season']
+euroleague_2018_2019_playerstats[['Fixture', 'Game']] = euroleague_2018_2019_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2018_2019_playerstats['Fixture']=pd.to_numeric(euroleague_2018_2019_playerstats['Fixture'])
+euroleague_2018_2019_playerstats['Round']=euroleague_2018_2019_playerstats['Fixture'].apply(fixture_format1)
+
+
+euroleague_2019_2020_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2019_2020_playerstats.csv")
+euroleague_2019_2020_playerstats['idseason']=euroleague_2019_2020_playerstats['IDGAME'] + "_" + euroleague_2019_2020_playerstats['Season']
+euroleague_2019_2020_playerstats[['Fixture', 'Game']] = euroleague_2019_2020_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2019_2020_playerstats['Fixture']=pd.to_numeric(euroleague_2019_2020_playerstats['Fixture'])
+euroleague_2019_2020_playerstats['Round']=euroleague_2019_2020_playerstats['Fixture'].apply(fixture_format3)
+
+
+euroleague_2020_2021_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2020_2021_playerstats.csv")
+euroleague_2020_2021_playerstats['idseason']=euroleague_2020_2021_playerstats['IDGAME'] + "_" + euroleague_2020_2021_playerstats['Season']
+euroleague_2020_2021_playerstats[['Fixture', 'Game']] = euroleague_2020_2021_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2020_2021_playerstats['Fixture']=pd.to_numeric(euroleague_2020_2021_playerstats['Fixture'])
+euroleague_2020_2021_playerstats['Round']=euroleague_2020_2021_playerstats['Fixture'].apply(fixture_format4)
+
+euroleague_2021_2022_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2021_2022_playerstats.csv")
+euroleague_2021_2022_playerstats['idseason']=euroleague_2021_2022_playerstats['IDGAME'] + "_" + euroleague_2021_2022_playerstats['Season']
+euroleague_2021_2022_playerstats[['Fixture', 'Game']] = euroleague_2021_2022_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2021_2022_playerstats['Fixture']=pd.to_numeric(euroleague_2021_2022_playerstats['Fixture'])
+euroleague_2021_2022_playerstats['Round']=euroleague_2021_2022_playerstats['Fixture'].apply(fixture_format4)
+
+
+euroleague_2022_2023_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2022_2023_playerstats.csv")
+euroleague_2022_2023_playerstats['idseason']=euroleague_2022_2023_playerstats['IDGAME'] + "_" + euroleague_2022_2023_playerstats['Season']
+euroleague_2022_2023_playerstats[['Fixture', 'Game']] = euroleague_2022_2023_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2022_2023_playerstats['Fixture']=pd.to_numeric(euroleague_2022_2023_playerstats['Fixture'])
+euroleague_2022_2023_playerstats['Round']=euroleague_2022_2023_playerstats['Fixture'].apply(fixture_format4)
+
+euroleague_2023_2024_playerstats=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2023_2024_playerstats.csv")
+euroleague_2023_2024_playerstats['idseason']=euroleague_2023_2024_playerstats['IDGAME'] + "_" + euroleague_2023_2024_playerstats['Season']
+euroleague_2023_2024_playerstats[['Fixture', 'Game']] = euroleague_2023_2024_playerstats['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2023_2024_playerstats['Fixture']=pd.to_numeric(euroleague_2023_2024_playerstats['Fixture'])
+euroleague_2023_2024_playerstats['Round']=euroleague_2023_2024_playerstats['Fixture'].apply(fixture_format5)
+
+
+
+
+euroleague_2016_2017_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2016_2017_results.csv")
+euroleague_2016_2017_results['idseason']=euroleague_2016_2017_results['IDGAME'] + "_" + euroleague_2016_2017_results['Season']
+euroleague_2016_2017_results[['Fixture', 'Game']] = euroleague_2016_2017_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2016_2017_results['Fixture']=pd.to_numeric(euroleague_2016_2017_results['Fixture'])
+euroleague_2016_2017_results['Round']=euroleague_2016_2017_results['Fixture'].apply(fixture_format1)
+
+
+euroleague_2017_2018_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2017_2018_results.csv")
+euroleague_2017_2018_results['idseason']=euroleague_2017_2018_results['IDGAME'] + "_" + euroleague_2017_2018_results['Season']
+euroleague_2017_2018_results[['Fixture', 'Game']] = euroleague_2017_2018_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2017_2018_results['Fixture']=pd.to_numeric(euroleague_2017_2018_results['Fixture'])
+euroleague_2017_2018_results['Round']=euroleague_2017_2018_results['Fixture'].apply(fixture_format2)
+
+
+euroleague_2018_2019_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2018_2019_results.csv")
+euroleague_2018_2019_results['idseason']=euroleague_2018_2019_results['IDGAME'] + "_" + euroleague_2018_2019_results['Season']
+euroleague_2018_2019_results[['Fixture', 'Game']] = euroleague_2018_2019_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2018_2019_results['Fixture']=pd.to_numeric(euroleague_2018_2019_results['Fixture'])
+euroleague_2018_2019_results['Round']=euroleague_2018_2019_results['Fixture'].apply(fixture_format1)
+
+
+euroleague_2019_2020_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2019_2020_results.csv")
+euroleague_2019_2020_results['idseason']=euroleague_2019_2020_results['IDGAME'] + "_" + euroleague_2019_2020_results['Season']
+euroleague_2019_2020_results[['Fixture', 'Game']] = euroleague_2019_2020_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2019_2020_results['Fixture']=pd.to_numeric(euroleague_2019_2020_results['Fixture'])
+euroleague_2019_2020_results['Round']=euroleague_2019_2020_results['Fixture'].apply(fixture_format3)
+
+
+euroleague_2020_2021_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2020_2021_results.csv")
+euroleague_2020_2021_results['idseason']=euroleague_2020_2021_results['IDGAME'] + "_" + euroleague_2020_2021_results['Season']
+euroleague_2020_2021_results[['Fixture', 'Game']] = euroleague_2020_2021_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2020_2021_results['Fixture']=pd.to_numeric(euroleague_2020_2021_results['Fixture'])
+euroleague_2020_2021_results['Round']=euroleague_2020_2021_results['Fixture'].apply(fixture_format4)
+
+euroleague_2021_2022_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2021_2022_results.csv")
+euroleague_2021_2022_results['idseason']=euroleague_2021_2022_results['IDGAME'] + "_" + euroleague_2021_2022_results['Season']
+euroleague_2021_2022_results[['Fixture', 'Game']] = euroleague_2021_2022_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2021_2022_results['Fixture']=pd.to_numeric(euroleague_2021_2022_results['Fixture'])
+euroleague_2021_2022_results['Round']=euroleague_2021_2022_results['Fixture'].apply(fixture_format4)
+
+
+euroleague_2022_2023_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2022_2023_results.csv")
+euroleague_2022_2023_results['idseason']=euroleague_2022_2023_results['IDGAME'] + "_" + euroleague_2022_2023_results['Season']
+euroleague_2022_2023_results[['Fixture', 'Game']] = euroleague_2022_2023_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2022_2023_results['Fixture']=pd.to_numeric(euroleague_2022_2023_results['Fixture'])
+euroleague_2022_2023_results['Round']=euroleague_2022_2023_results['Fixture'].apply(fixture_format4)
+
+euroleague_2023_2024_results=pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/euroleague/main/euroleague_2023_2024_results.csv")
+euroleague_2023_2024_results['idseason']=euroleague_2023_2024_results['IDGAME'] + "_" + euroleague_2023_2024_results['Season']
+euroleague_2023_2024_results[['Fixture', 'Game']] = euroleague_2023_2024_results['IDGAME'].str.split('_', n=1, expand=True)
+euroleague_2023_2024_results['Fixture']=pd.to_numeric(euroleague_2023_2024_results['Fixture'])
+euroleague_2023_2024_results['Round']=euroleague_2023_2024_results['Fixture'].apply(fixture_format5)
+
+
+All_Seasons=pd.concat([euroleague_2016_2017_playerstats,euroleague_2017_2018_playerstats,euroleague_2018_2019_playerstats,euroleague_2019_2020_playerstats,euroleague_2020_2021_playerstats,euroleague_2021_2022_playerstats,euroleague_2022_2023_playerstats,euroleague_2023_2024_playerstats])
+
+All_Seasons_res=pd.concat([euroleague_2016_2017_results,euroleague_2017_2018_results,euroleague_2018_2019_results,euroleague_2019_2020_results,euroleague_2020_2021_results,euroleague_2021_2022_results,euroleague_2022_2023_results,euroleague_2023_2024_results])
+
+st.write("# Euroleague Stats from 2016-2017 to present")
+
+kpi1, kpi2, kpi3, kpi4,kpi5 = st.columns(5)
+with kpi1:
+    st.markdown(lnk + metrics_customize(240,153,48,"","Total Games",All_Seasons['idseason'].nunique()), unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(240,153,48,"","Total Teams",All_Seasons['Team'].nunique()), unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(240,153,48, "", "Total Players", All_Seasons['Player'].nunique()),
+        unsafe_allow_html=True)
+
+with kpi2:
+    st.markdown(lnk + metrics_customize(240,153,48, "", "Total Points", All_Seasons['PTS'].sum()),
+                unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(240,153,48, "", "Total Assists", All_Seasons['AS'].sum()),
+                unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(240,153,48, "", "Total Steals", All_Seasons['ST'].sum()),
+                unsafe_allow_html=True)
+
+with kpi3:
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total 3P Points Made", All_Seasons['F3M'].sum()),
+        unsafe_allow_html=True)
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total 2P Points Made", All_Seasons['F2M'].sum()),
+        unsafe_allow_html=True)
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total Free Throws Points Made",
+                                All_Seasons['FTM'].sum()),
+        unsafe_allow_html=True)
+
+with kpi4:
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total Turnovers", All_Seasons['TO'].sum()),
+        unsafe_allow_html=True)
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total Blocks", All_Seasons['BLK'].sum()),
+        unsafe_allow_html=True)
+    st.markdown(
+        lnk + metrics_customize(240,153,48, "", "Total Fouls",
+                                All_Seasons['PF'].sum()),
+        unsafe_allow_html=True)
+

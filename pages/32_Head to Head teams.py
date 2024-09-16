4be9231ff6f5d88fa4a6749f70d6d8c2378a599e
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-09-16 16:55:06 +0300

    Update 32_Head to Head teams.py

--- pages/32_Head to Head teams.py
+++ pages/32_Head to Head teams.py
@@ -419,7 +419,7 @@ def team_rating_stat_higher(dataset,stat):
     return final_dataset
 
 def team_rating_stat_lower(dataset,stat):
-    dataset1=dataset[["Team",stat]].sort_values(stat,ascending=False).reset_index()
+    dataset1=dataset[["Team",stat]].sort_values(stat,ascending=True).reset_index()
     dataset1.drop("index",axis=1,inplace=True)
     final_dataset=dataset1.reset_index() >> mutate(Rating=(100*(X.index+1)/X.Team.nunique()),Rating1=(100-(100-X.Rating.round(0))*0.5).round(0))
     final_dataset.rename(columns={'Rating1':'Rating '+ stat},inplace=True)

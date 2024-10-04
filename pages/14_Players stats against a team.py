395d55a3c483eea84eedc751daef0f4375e0b381
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-04 10:45:15 +0300

    Update 14_Players stats against a team.py

--- pages/14_Players stats against a team.py
+++ pages/14_Players stats against a team.py
@@ -209,7 +209,7 @@ All_Seasons["Win"]=All_Seasons['results'].apply(wins_against_format)
 
 player_stats_by_team_team=st.sidebar.selectbox("Choose Team:",All_Seasons['Team'].reset_index().sort_values('Team')['Team'].unique())
 player_stats_by_team_ha = st.sidebar.selectbox("Home or Away games(of Team):",['A', 'H', 'All'],index=2)
-player_stats_by_team_season = st.sidebar.selectbox("Season:",['2016-2017', '2017-2018', '2018-2019', '2019-2020', '2020-2021','2021-2022', '2022-2023', '2023-2024','All'],index=8)
+player_stats_by_team_season = st.sidebar.selectbox("Season:",['2016-2017', '2017-2018', '2018-2019', '2019-2020', '2020-2021','2021-2022', '2022-2023', '2023-2024','2024-2025','All'],index=8)
 player_stats_by_team_phase = st.sidebar.selectbox("Phase:",['Regular Season', 'Play In','Play offs', 'Final Four','All'],index=4)
 player_stats_by_team_wl = st.sidebar.selectbox("Result(of Team):",['W', 'L','All'],index=2)
 player_stats_by_team_round = st.sidebar.selectbox("Round:",['First Round', 'Second Round','PI 1', 'PI 2', 'PO 1', 'PO 2', 'PO 3', 'PO 4','PO 5', 'Semi Final', 'Third Place', 'Final', 'All'],index=12)
@@ -225,7 +225,7 @@ else:
     select_ha_player1 = player_stats_by_team_ha
 
 if "All" in player_stats_by_team_season:
-    player_stats_by_team_season = ['2016-2017', '2017-2018', '2018-2019', '2019-2020','2020-2021','2021-2022', '2022-2023','2023-2024']
+    player_stats_by_team_season = ['2016-2017', '2017-2018', '2018-2019', '2019-2020','2020-2021','2021-2022', '2022-2023','2023-2024','2024-2025']
     All_Seasons2=All_Seasons2.loc[All_Seasons2['Season'].isin(player_stats_by_team_season)]
     select_season_player1 = ''
 else:

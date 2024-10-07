c7dfde94f64601f3219c7033d502051af5d49b7a
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-07 12:23:43 +0300

    Update 30_Team Rankings.py

--- pages/30_Team Rankings.py
+++ pages/30_Team Rankings.py
@@ -20,6 +20,11 @@ from plotly.subplots import make_subplots
 
 
 st.set_page_config(layout='wide',page_title="Team Rankings",page_icon="🏀")
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
+
+download_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6BYfXG_CbAZa1KEDq4pu2Fxw_OeTLLmFcA&s','eurologo.png')
+st.image(Image.open("eurologo.png"),width=100)
 def fixture_format1(Fixture):
     if Fixture<=15:
         return "First Round"

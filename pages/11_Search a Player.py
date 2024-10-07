82c0287e85f813489f9516394f2c9d9ac5b0c3a8
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-07 12:16:48 +0300

    Update 11_Search a Player.py

--- pages/11_Search a Player.py
+++ pages/11_Search a Player.py
@@ -20,6 +20,11 @@ from streamlit.components.v1 import html
 
 
 st.set_page_config(layout='wide',page_title="Search a Player",page_icon="🏀")
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
+
+download_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6BYfXG_CbAZa1KEDq4pu2Fxw_OeTLLmFcA&s','eurologo.png')
+st.image(Image.open("eurologo.png"),width=100)
 def fixture_format1(Fixture):
     if Fixture<=15:
         return "First Round"

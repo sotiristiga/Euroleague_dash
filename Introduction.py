5af4369213824678406ed39c08ca15df6607ff05
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-07 11:50:46 +0300

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -18,7 +18,7 @@ st.set_page_config(layout='wide',page_title="Euroleague",page_icon="🏀")
 def download_image(url, save_as):
     urllib.request.urlretrieve(url, save_as)
 
-download_image('https://iconape.com/wp-content/files/uv/327099/svg/327099.svg','eurologo.png')
+download_image('https://media-cdn.incrowdsports.com/23610a1b-1c2e-4d2a-8fe4-ac2f8e400632.svg','eurologo.png')
 st.image(Image.open("eurologo.png"),width=400)
 def fixture_format1(Fixture):
     if Fixture<=15:

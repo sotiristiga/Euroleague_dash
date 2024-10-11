d56e2eb2feb61578ed0af4e6305f41b578783f78
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-11 12:15:05 +0300

    Update 2_Find stats for a game.py

--- pages/2_Find stats for a game.py
+++ pages/2_Find stats for a game.py
@@ -25,6 +25,7 @@ def download_image(url, save_as):
 
 download_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6BYfXG_CbAZa1KEDq4pu2Fxw_OeTLLmFcA&s','eurologo.png')
 st.image(Image.open("eurologo.png"),width=100)
+st.sidebar.write("If an error message appears, please refresh the page")
 def fixture_format1(Fixture):
     if Fixture<=15:
         return "First Round"

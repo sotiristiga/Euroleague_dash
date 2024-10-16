315902118ede8da16e20129efb1ff6a0443dbf10
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-10-16 12:44:52 +0300

    Update 11_Search a Player.py

--- pages/11_Search a Player.py
+++ pages/11_Search a Player.py
@@ -20,6 +20,7 @@ from streamlit.components.v1 import html
 
 
 st.set_page_config(layout='wide',page_title="Search a Player",page_icon="🏀")
+st.sidebar.write("If an error message appears, please refresh the page")
 def download_image(url, save_as):
     urllib.request.urlretrieve(url, save_as)
 

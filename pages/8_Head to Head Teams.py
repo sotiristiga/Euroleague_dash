49ebe1ca2e63d6693cabc3df4cc8693cc0460c80
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-11-18 20:32:38 +0200

    Update 8_Head to Head Teams.py

--- pages/8_Head to Head Teams.py
+++ pages/8_Head to Head Teams.py
@@ -21,8 +21,11 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(layout='wide', page_title="Head to Head teams", page_icon="🏀")
 st.sidebar.write("If an error message appears, please refresh the page")
-st.write("## Euroleague stats from 2017 to present")
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
 
+download_image('https://raw.githubusercontent.com/sotiristiga/Euroleague_dash/refs/heads/main/eurologo.png','eurologo.png')
+st.image(Image.open("eurologo.png"),width=100)
 def fixture_format1(Fixture):
     if Fixture <= 15:
         return "First Round"

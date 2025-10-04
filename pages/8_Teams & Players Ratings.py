624f3591687054b00660d6f3997449ce6f5f98c7
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2025-10-04 23:38:20 +0300

    Update 8_Teams & Players Ratings.py

--- pages/8_Teams & Players Ratings.py
+++ pages/8_Teams & Players Ratings.py
@@ -270,7 +270,7 @@ st.sidebar.markdown('''
 st.header("Filters")
 f1,f2,f3,f4,f5=st.columns(5)
 with f1:
-    selected_season = st.selectbox("Season:",['2016-2017', '2017-2018', '2018-2019', '2019-2020', '2020-2021','2021-2022', '2022-2023', '2023-2024','2024-2025','2025-2026','All'],index=8)
+    selected_season = st.selectbox("Season:",['2016-2017', '2017-2018', '2018-2019', '2019-2020', '2020-2021','2021-2022', '2022-2023', '2023-2024','2024-2025','2025-2026','All'],index=9)
 with f2:
     selected_phase = st.selectbox("Phase:",['Regular Season', 'Play In','Play offs', 'Final Four','All'],index=4)
 with f3:
